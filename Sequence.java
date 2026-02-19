c82d6b791d03b67a323e1a67d1772d475be7eb0a
mohammad27hasan <mohammad_hasan27@outlook.com>
2026-02-19 06:17:22 -0800

    add Sequence v0.0.8

--- Sequence.java
+++ Sequence.java
@@ -1,118 +1,131 @@
 /*
-* Class: Sorting algorithm
-* Data structure: Array
-* Version: 0.0.7
+* Algorithms: Insertion sort, Reverse
+* Abstract data type: Sequence
+* Version: 0.0.8
 * Author: Mohammad Hasan
 */
 package algo;
 
 public final class Sequence {
-    private static int i;
-    private static int j;
+    private static int start;
+    private static int end;
 
     private Sequence() {}
 
     public static void insertionSort(byte[] array) {
         byte key = 0;
-        for (j = 1; j < array.length; j++) {
-            key = array[j];
-            i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
-                array[i + 1] = array[i];
-                i--;
+        for (end = 1; end < array.length; end++) {
+            key = array[end];
+            start = end - 1;
+            while ((start > -1) && (array[start] > key)) {
+                array[start + 1] = array[start];
+                start--;
             }
-            array[i + 1] = key;
+            array[start + 1] = key;
         }
     }
 
     public static void insertionSort(char[] array) {
         char key = '0';
-        for (j = 1; j < array.length; j++) {
-            key = array[j];
-            i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
-                array[i + 1] = array[i];
-                i--;
+        for (end = 1; end < array.length; end++) {
+            key = array[end];
+            start = end - 1;
+            while ((start > -1) && (array[start] > key)) {
+                array[start + 1] = array[start];
+                start--;
             }
-            array[i + 1] = key;
+            array[start + 1] = key;
         }
     }
 
     public static void insertionSort(double[] array) {
         double key = 0.0;
-        for (j = 1; j < array.length; j++) {
-            key = array[j];
-            i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
-                array[i + 1] = array[i];
-                i--;
+        for (end = 1; end < array.length; end++) {
+            key = array[end];
+            start = end - 1;
+            while ((start > -1) && (array[start] > key)) {
+                array[start + 1] = array[start];
+                start--;
             }
-            array[i + 1] = key;
+            array[start + 1] = key;
         }
     }
 
     public static void insertionSort(float[] array) {
         float key = 0.0f;
-        for (j = 1; j < array.length; j++) {
-            key = array[j];
-            i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
-                array[i + 1] = array[i];
-                i--;
+        for (end = 1; end < array.length; end++) {
+            key = array[end];
+            start = end - 1;
+            while ((start > -1) && (array[start] > key)) {
+                array[start + 1] = array[start];
+                start--;
             }
-            array[i + 1] = key;
+            array[start + 1] = key;
         }
     }
 
     public static void insertionSort(int[] array) {
         int key = 0;
-        for (j = 1; j < array.length; j++) {
-            key = array[j];
-            i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
-                array[i + 1] = array[i];
-                i--;
+        for (end = 1; end < array.length; end++) {
+            key = array[end];
+            start = end - 1;
+            while ((start > -1) && (array[start] > key)) {
+                array[start + 1] = array[start];
+                start--;
             }
-            array[i + 1] = key;
+            array[start + 1] = key;
         }
     }
 
     public static void insertionSort(long[] array) {
         long key = 0L;
-        for (j = 1; j < array.length; j++) {
-            key = array[j];
-            i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
-                array[i + 1] = array[i];
-                i--;
+        for (end = 1; end < array.length; end++) {
+            key = array[end];
+            start = end - 1;
+            while ((start > -1) && (array[start] > key)) {
+                array[start + 1] = array[start];
+                start--;
             }
-            array[i + 1] = key;
+            array[start + 1] = key;
         }
     }
 
     public static void insertionSort(short[] array) {
         short key = 0;
-        for (j = 1; j < array.length; j++) {
-            key = array[j];
-            i = j - 1;
-            while ((i > -1) && (array[i] > key)) {
-                array[i + 1] = array[i];
-                i--;
+        for (end = 1; end < array.length; end++) {
+            key = array[end];
+            start = end - 1;
+            while ((start > -1) && (array[start] > key)) {
+                array[start + 1] = array[start];
+                start--;
             }
-            array[i + 1] = key;
+            array[start + 1] = key;
         }
     }
 
     public static <T extends Comparable<T>> void insertionSort(T[] array) {
         T key = null;
-        for (j = 1; j < array.length; j++) {
-            key = array[j];
-            i = j - 1;
-            while ((i > -1) && (array[i].compareTo(key) > 0)) {
-                array[i + 1] = array[i];
-                i--;
+        for (end = 1; end < array.length; end++) {
+            key = array[end];
+            start = end - 1;
+            while ((start > -1) && (array[start].compareTo(key) > 0)) {
+                array[start + 1] = array[start];
+                start--;
             }
-            array[i + 1] = key;
+            array[start + 1] = key;
+        }
+    }
+
+    public static void reverse(byte[] array) {
+        byte temp = 0;
+        start = 0;
+        end = array.length - 1;
+        while (start < end) {
+            temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
         }
     }
 }
