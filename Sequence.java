8c0af4afac847823e8a7b2f2ec00f08f51c21eec
mohammad27hasan <mohammad_hasan27@outlook.com>
2026-03-27 07:22:19 -0800

    add Sequence v0.1.6

--- Sequence.java
+++ Sequence.java
@@ -1,7 +1,7 @@
 /*
 * Algorithms: Insertion sort
 * Abstract data type: Sequence
-* Version: 0.1.5
+* Version: 0.1.6
 * Author: Mohammad Hasan
 */
 package algo;
@@ -186,8 +186,20 @@ public final class Sequence {
         insertionSort(array, false);
     }
 
-    public static void insertionSort(short[] array) {
+    public static void insertionSort(short[] array, boolean nonincreasingOrder) {
         short key = 0;
+        if (nonincreasingOrder) {
+            for (end = 1; end < array.length; end++) {
+                key = array[end];
+                start = end - 1;
+                while ((start > -1) && (array[start] < key)) {
+                    array[start + 1] = array[start];
+                    start--;
+                }
+                array[start + 1] = key;
+            }
+            return;
+        }
         for (end = 1; end < array.length; end++) {
             key = array[end];
             start = end - 1;
@@ -197,6 +209,10 @@ public final class Sequence {
             }
             array[start + 1] = key;
         }
+    }
+
+    public static void insertionSort(short[] array) {
+        insertionSort(array, false);
     }
 
     public static <T extends Comparable<T>> void insertionSort(T[] array) {
